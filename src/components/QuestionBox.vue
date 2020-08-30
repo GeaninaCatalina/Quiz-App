a378567380c7c15511e385e8524169cbf34d17ae
GeaninaCatalina <masgras.geanina@hotmail.com>
2020-08-30 18:41:58 +0200

    added css to quextion box component

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="question-box-container">
     <b-jumbotron>
-      <template v-slot:lead>
-        {{ currentQuestion.question}}
-      </template>
+      <template v-slot:lead>{{ currentQuestion.question}}</template>
 
       <hr class="my-4" />
-
-      <p v-for="(answer, index) in showAnswers" :key="index">
-        {{answer}}
-      </p>
+      <b-list-group>
+        <b-list-group-item 
+        v-for="(answer, index) in showAnswers" 
+        :key="index"
+        >
+          {{answer}}
+        </b-list-group-item>
+      </b-list-group>
 
       <b-button variant="primary" href="#">Submit</b-button>
-      <b-button v-on:click="onClicNext" variant="success" href="#">
-        Next
-      </b-button>
+      <b-button v-on:click="onClicNext" variant="success" href="#">Next</b-button>
     </b-jumbotron>
   </div>
 </template> 
@@ -24,13 +24,26 @@ export default {
   props: {
     currentQuestion: Object,
     onClicNext: Function
-  }, 
+  },
   computed: {
     showAnswers() {
-      let answers = [...this.currentQuestion.incorrect_answers]
-      answers.push(this.currentQuestion.correct_answer)
-      return answers; 
+      let answers = [...this.currentQuestion.incorrect_answers];
+      answers.push(this.currentQuestion.correct_answer);
+      return answers;
     }
   }
-}
+};
 </script>
+
+<style scoped>
+.list-group {
+  margin-bottom: 15px;
+}
+.list-group-item:hover {
+  background: #eee;
+  cursor: pointer;
+}
+.btn {
+  margin: 0px 5px;
+}
+</style>
