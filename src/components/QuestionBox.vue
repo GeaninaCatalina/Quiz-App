ed0a9a076ae9fee82d498ecde9d38062f0291734
GeaninaCatalina <masgras.geanina@hotmail.com>
2020-08-30 18:16:59 +0200

    Create component QuestionBox

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,11 +1,36 @@
 <template>
   <div class="question-box-container">
     <b-jumbotron>
-      <template v-slot:lead>Insert questions here.</template>
+      <template v-slot:lead>
+        {{ currentQuestion.question}}
+      </template>
+
       <hr class="my-4" />
-      <p>Insert list of answeres here.</p>
+
+      <p v-for="(answer, index) in showAnswers" :key="index">
+        {{answer}}
+      </p>
+
       <b-button variant="primary" href="#">Submit</b-button>
-      <b-button variant="success" href="#">Next</b-button>
+      <b-button v-on:click="onClicNext" variant="success" href="#">
+        Next
+      </b-button>
     </b-jumbotron>
   </div>
-</template>
+</template> 
+
+<script>
+export default {
+  props: {
+    currentQuestion: Object,
+    onClicNext: Function
+  }, 
+  computed: {
+    showAnswers() {
+      let answers = [...this.currentQuestion.incorrect_answers]
+      answers.push(this.currentQuestion.correct_answer)
+      return answers; 
+    }
+  }
+}
+</script>
