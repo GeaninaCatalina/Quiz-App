3c73dd4f8512b7b71ee19c36b326e5565dd9675e
GeaninaCatalina <masgras.geanina@hotmail.com>
2020-08-30 14:41:53 +0200

    Create Header compnent

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <div>
     <b-nav tabs>
       <b-nav-item disabled><b>Quiz App</b></b-nav-item>
-      <b-nav-item disabled>Counter: 10/12</b-nav-item>
+      <b-nav-item disabled><b>ounter: 10/12</b></b-nav-item>
     </b-nav>
   </div>
 </template>
