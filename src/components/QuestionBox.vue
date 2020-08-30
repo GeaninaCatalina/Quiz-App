624296c0da14cf8276b672a3433cd66df49ef470
GeaninaCatalina <masgras.geanina@hotmail.com>
2020-08-30 15:08:56 +0200

    Create layout for QuestionBox Component

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,3 +1,11 @@
 <template>
-    <h1>Question Box</h1>
+  <div class="question-box-container">
+    <b-jumbotron>
+      <template v-slot:lead>Insert questions here.</template>
+      <hr class="my-4" />
+      <p>Insert list of answeres here.</p>
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
 </template>
